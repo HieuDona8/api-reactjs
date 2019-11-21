7f0f2323f4c0f9a83cce75f7dd54e572fa1c56a5
TrungHieuHo <hieu.ho@journeyh.io>
2019-11-21 23:51:32 +0700

    add products 1

--- src/pages/ProductActionPage/ProductActionPage.js
+++ src/pages/ProductActionPage/ProductActionPage.js
@@ -1,23 +1,71 @@
 import React, { Component } from "react";
-
+import callApi from './../../utils/apiCaller';
 class ProductActionPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      txtName: "",
+      txtPrice: "",
+      chkbStatus: ""
+    };
+  }
+
+  onChange = (e) =>{
+    const target = e.target;
+    const name = target.name;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onSave = (e)=>{
+    e.preventDefault();
+    const { txtName, txtPrice, chkbStatus} = this.state;
+    callApi('products', 'POST', {
+      name : txtName,
+      price: txtPrice,
+      status: chkbStatus,
+    }).then(res =>{
+      console.log("my", res)
+    })
+  }
   render() {
+    const {txtName, txtPrice, chkbStatus} = this.state;
     return (
       <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-        <form>
+        <form onSubmit={this.onSave}>
           <div className="form-group">
             <label>Tên Sản Phẩm</label>
-            <input type="text" className="form-control" />
+            <input 
+              type="text" 
+              className="form-control" 
+              name="txtName" 
+              value={txtName}
+              onChange={this.onChange}
+            />
           </div>
           <div className="form-group">
             <label>Giá</label>
-            <input type="number" className="form-control" />
+            <input 
+              type="number" 
+              className="form-control" 
+              name="txtPrice" 
+              value={txtPrice}
+              onChange={this.onChange}
+            />
           </div>
           <div className="form-group">
-            <label>Trạng Thái</label>            
+            <label>Trạng Thái</label>
             <div className="checkbox">
               <label>
-                <input type="checkbox"/>
+                <input 
+                  type="checkbox" 
+                  name="chkbStatus" 
+                  value={chkbStatus}
+                  onChange={this.onChange}
+                />
                 Còn Hàng
               </label>
             </div>
--- src/pages/ProductListPage/ProductListPage.js
+++ src/pages/ProductListPage/ProductListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ProductList from "../../components/ProductList/ProductList";
 import ProductItem from "../../components/ProductItem/ProductItem";
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux'
 import callApi from "./../../utils/apiCaller";
 class ProductListPage extends Component {
@@ -24,9 +25,9 @@ class ProductListPage extends Component {
     const {products} = this.state;
     return (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <button type="button" className="btn btn-info mb-10">
+        <Link to="/product/add" className="btn btn-info mb-10">
           Thêm Sản Phẩm
-        </button>
+        </Link>
         <ProductList>
           {this.showProducts(products)}
         </ProductList>        
