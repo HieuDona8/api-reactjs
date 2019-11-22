992a0ab399252b99a1ea448ce9faa1f1aca66c91
TrungHieuHo <hieu.ho@journeyh.io>
2019-11-22 15:31:02 +0700

    built-in middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import appReducers from './reducers/index';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   appReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  //applyMiddleware(thunk)
+  composeEnhancer(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
